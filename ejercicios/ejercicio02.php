a5699ecae1167614f3799499ebcc796fd20824be
Pablo Marco <pablomarcocastillo@gmail.com>
2020-09-23 20:18:45 +0100

    ejercicio02

---
+++ ejercicios/ejercicio02.php
@@ -0,0 +1,17 @@
+<html>
+    <h1>Ejercicio02: Imprimir la tabla de multiplicar del 8</h1>
+
+    <?php
+
+    const multiplo=8;
+    
+
+    for ($i=0; $i <=10 ; $i++) 
+    { 
+        
+        echo $i . ' x ' . multiplo . ' = ' . ($i * multiplo) . '<br/>';
+    }
+
+    ?>
+
+</html>
